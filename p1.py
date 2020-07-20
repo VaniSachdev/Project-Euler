fd2ee2a7a58316a6367c9251eac8146edfb4eef8
Vani Sachdev <sachdev.vani@gmail.com>
2020-07-19 21:02:39 -0700

    added p1 and p2

---
+++ p1.py
@@ -0,0 +1,14 @@
+
+''' project euler 
+multiples of 3 & 5
+problem 1 '''
+
+def math():
+  list = []
+  for x in range(1000):
+    if (x%3 == 0 or x%5 == 0):
+      list.append(x)
+  print(sum(list))
+
+if __name__ == "__main__":
+  math()
---
+++ p2.py
@@ -0,0 +1,30 @@
+
+''' project euler 
+even fibonacci numbers
+problem 2 '''
+
+# 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
+
+#recursion was too slow 
+# def r_fib(n):
+# 	if n <= 2:
+# 		return n 
+# 	else: 
+# 		return r_fib(n-1)+ r_fib(n-2) 
+	
+	 
+def fib():
+	fib_total = 0
+	x, y = 1, 2  
+	
+	while x <= 4e6:
+		if x % 2 == 0:
+			fib_total += x
+		x = y
+		y = x + y
+
+	return fib_total
+
+
+if __name__ == "__main__":
+	print(fib())
