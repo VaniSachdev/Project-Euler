21a255c9b24b37c110299ec2790df9bcb65c1424
Vani Sachdev <sachdev.vani@gmail.com>
2020-08-06 00:52:36 -0700

    added recap

--- psets/p15.py
+++ psets/p15.py
@@ -6,12 +6,16 @@
 # https://en.wikipedia.org/wiki/Binomial_coefficient
 
 #assumes start pos @ (0,0)
-
+import time
 import math 
 def path(gridx, gridy):
+    start = time.time()
     answer = (math.factorial(gridx+gridy)) / (math.factorial(gridx) * math.factorial(gridy))
     print (int(answer))
+    end = time.time()
+    print (end-start)
 
 if __name__ == "__main__": 
     path(20, 20)
+
   
